16446316ccea9bb69565ab6dbe87f64c677ba369
svladislav00-qq <svladislav00@gmail.com>
2026-03-09 19:26:03 +0300

    created client file for auth service

--- auth/client.go
+++ auth/client.go
@@ -1,1 +1,90 @@
 package auth
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/svladislav00-qq/event-microservices/auth/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Client struct {
+	conn    *grpc.ClientConn
+	service pb.AuthServiceClient
+}
+
+func NewClient(url string) (*Client, error) {
+	conn, err := grpc.NewClient(url, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+	c := pb.NewAuthServiceClient(conn)
+	return &Client{
+		conn:    conn,
+		service: c,
+	}, nil
+}
+
+func (c *Client) Close() {
+	c.conn.Close()
+}
+
+func (c *Client) Register(ctx context.Context, email string, password string, username string) (*Account, error) {
+	r, err := c.service.Register(
+		ctx,
+		&pb.RegisterRequest{
+			Email:    email,
+			Password: password,
+			Username: username,
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+	if r == nil {
+		return nil, fmt.Errorf("empty account in response")
+	}
+	return &Account{
+		ID:        r.Account.Id,
+		Email:     r.Account.Email,
+		Username:  r.Account.Username,
+		Role:      r.Account.Role.String(),
+		CreatedAt: r.Account.CreatedAt.AsTime(),
+	}, nil
+}
+
+func (c *Client) Login(ctx context.Context, email, password string) (string, error) {
+	r, err := c.service.Login(ctx, &pb.LoginRequest{
+		Email:    email,
+		Password: password,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return r.Token, nil
+}
+
+func (c *Client) PromoteToModerator(ctx context.Context, userID string, departmentID string) (*Account, error) {
+	r, err := c.service.PromoteToModerator(ctx, &pb.PromoteToModeratorRequest{
+		UserId:       userID,
+		DepartmentId: departmentID,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	acc := r.GetAccount()
+	if acc == nil {
+		return nil, fmt.Errorf("empty account in response")
+	}
+	return &Account{
+		ID:         acc.Id,
+		Email:      acc.Email,
+		Username:   acc.Username,
+		Role:       acc.Role.String(),
+		Department: acc.DepartmentId,
+		CreatedAt:  acc.CreatedAt.AsTime(),
+	}, nil
+}
--- go.mod
+++ go.mod
@@ -4,9 +4,11 @@ go 1.25.5
 
 require (
 	github.com/99designs/gqlgen v0.17.87
+	github.com/golang-jwt/jwt/v5 v5.3.1
 	github.com/joho/godotenv v1.5.1
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/minio/minio-go/v7 v7.0.99
+	github.com/segmentio/ksuid v1.0.4
 	github.com/vektah/gqlparser/v2 v2.5.32
 	google.golang.org/grpc v1.79.2
 	google.golang.org/protobuf v1.36.11
@@ -17,12 +19,10 @@ require (
 require (
 	github.com/agnivade/levenshtein v1.2.1 // indirect
 	github.com/go-viper/mapstructure/v2 v2.5.0 // indirect
-	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/segmentio/ksuid v1.0.4 // indirect
 	github.com/sosodev/duration v1.3.1 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20251202230838-ff82c1b0f217 // indirect
 )
@@ -49,7 +49,7 @@ require (
 	github.com/rs/xid v1.6.0 // indirect
 	github.com/tinylib/msgp v1.6.1 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
-	golang.org/x/crypto v0.48.0 // indirect
+	golang.org/x/crypto v0.48.0
 	golang.org/x/net v0.50.0 // indirect
 	golang.org/x/sync v0.19.0 // indirect
 	golang.org/x/sys v0.41.0 // indirect
